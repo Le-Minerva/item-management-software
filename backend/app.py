e04bde1e71ecce1d6d5c6f1aef69bf71b47999aa
Le-Minerva <chengamess@163.com>
2025-10-21 12:59:50 +0800

    first trial

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,19 @@
+import time
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route('/api')
+def hello_world():
+    return 'Hello, from Flask!'
+
 @app.route("/api/hello")
 def hello():
     return jsonify({"message": "Hello from Flask!"})
+
+@app.route('/api/time')
+def get_current_time():
+    return {'time': time.time()}
 
 if __name__ == "__main__":
     app.run(debug=True)
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,11 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    port: 5173,      // fix the port used to run the react app
+    strictPort: true, // raise error when th default port is occupied instead of changing one
+    proxy: {
+      '/api': 'http://localhost:5000',
+    }, //any incoming requests that have a URL that starts with /api have to be forwarded to http://localhost:5000
+  }
 })
